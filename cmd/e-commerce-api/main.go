1e97aea74c479aeaa84a18e5c3396630b8d68590
Hafidz Athaya <mhafidza528@gmail.com>
2026-04-14 13:47:44 +0800

    add: initial server using chi router

--- cmd/e-commerce-api/main.go
+++ cmd/e-commerce-api/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	log.Println("Hello e-commerce")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", r)
 }
