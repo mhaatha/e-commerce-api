ed70824d7f60e5edc9818e7f84aa4037b026fca1
Hafidz Athaya <mhafidza528@gmail.com>
2026-04-25 13:00:59 +0800

    add: product model

---
+++ internal/model/domain/product.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type Product struct {
+	Id          string    `json:"id"`
+	Name        string    `json:"name"`
+	Quantity    int       `json:"quantity"`
+	Price       int       `json:"price"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
