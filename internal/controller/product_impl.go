6ea6532b584772afbb0aec3ed589fae0fea3566e
Hafidz Athaya <mhafidza528@gmail.com>
2026-04-29 20:23:05 +0800

    feat(api): [WIP] implement basic create controller

--- internal/controller/product_impl.go
+++ internal/controller/product_impl.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"encoding/json"
+	"io"
+	"log/slog"
 	"net/http"
 
+	"github.com/mhaatha/go-e-commerce-api/internal/model/web"
 	"github.com/mhaatha/go-e-commerce-api/internal/service"
 )
 
@@ -16,4 +20,25 @@ func NewProductController(productService service.ProductService) ProductControll
 	}
 }
 
-func (controller *ProductControllerImpl) Create(w http.ResponseWriter, r *http.Request) {}
+func (controller *ProductControllerImpl) Create(w http.ResponseWriter, r *http.Request) {
+	var bodyBytes []byte
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		slog.Error("error when read the request body", "err", err)
+	}
+
+	var productRequest web.CreateProductRequest
+	if err := json.Unmarshal(bodyBytes, &productRequest); err != nil {
+		slog.Error("error when unmarshal the JSON", "err", err)
+	}
+
+	productResponse, err := controller.productService.CreateNewProduct(r.Context(), productRequest)
+	if err != nil {
+		slog.Error("error when calling the create new product service", "err", err)
+	}
+
+	jsonResponse, err := json.Marshal(productResponse)
+	if err != nil {
+		slog.Error("error when marshal the JSON", "err", err)
+	}
+}
